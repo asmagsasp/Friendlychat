9d64849387c46127f0b444e029afb15e42d32007
Abel S Magalhães <abel.souza.magalhaes@hotmail.com>
2018-07-12 19:55:32 -0400

    Adicionando uma interface do usuário para redigir mensagens

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,72 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+// Modify the ChatScreen class definition to extend StatefulWidget.
+
+class ChatScreen extends StatefulWidget {
   @override
+  State createState() => new ChatScreenState();
+
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: Text("Friendlychat"),
       ),
+    );
+  }
+}
+
+// Add the ChatScreenState class definition in main.dart.
+class ChatScreenState extends State<ChatScreen> {
+  // Para gerenciar as interações com o campo de texto
+  final TextEditingController _textController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("Friendlychat"),
+      ),
+      body: _buildTextComposer(),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear;
+  }
+
+  //
+  //    Adicione um TextFieldwidget e configure-o da seguinte forma para gerenciar as interações do usuário:
+
+  //    Para ter controle sobre o conteúdo do campo de texto, forneceremos ao TextFieldconstrutor um TextEditingController.
+  //    Este controlador também pode ser usado para limpar o campo ou ler seu valor.
+  //    Para ser notificado quando o usuário enviar uma mensagem, use o onSubmittedargumento para fornecer um método de retorno de chamada particular _handleSubmitted().
+  //    Por enquanto, esse método apenas limpará o campo e, posteriormente, adicionaremos mais código para enviar a mensagem. Defina este método da seguinte maneira:
+  //
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+        data: IconThemeData(color: Theme.of(context).accentColor),
+        child: new Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: new Row(
+                children: <Widget>[
+              new Flexible(
+                child: TextField(
+                  controller: _textController,
+                  onSubmitted: _handleSubmitted,
+                  decoration:
+                      new InputDecoration.collapsed(hintText: "Send a message"),
+                ),
+              ),
+              new Container(
+                  margin: new EdgeInsets.symmetric(horizontal: 4.0),
+                  child: new IconButton(
+                      icon: new Icon(Icons.send),
+                      onPressed: () => _handleSubmitted(_textController.text))),
+            ]
+            )
+        )
     );
   }
 }
