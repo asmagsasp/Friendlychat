8a38e273e8a87b3ee98870fb4e830a2b2b955d9c
Abel S Magalhães <abel.souza.magalhaes@hotmail.com>
2018-07-12 18:22:43 -0400

    Construindo a interface do usuário principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(
-    new MaterialApp(
+void main() {
+  runApp(new FriendlychatApp());
+}
+
+class FriendlychatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
       title: "Friendlychat",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Friendlychat'),
-        ),
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text("Friendlychat"),
       ),
-    )
-  );
+    );
+  }
 }
